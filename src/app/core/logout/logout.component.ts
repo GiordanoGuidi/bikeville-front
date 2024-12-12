cb89c4df4eb8cee329beb1ff2ebf7fe7592139c6
Serapheenee <davideserafini.02@gmail.com>
2024-12-12 15:28:43 +0100

    Home, AdminProducts e Routing

--- src/app/core/logout/logout.component.ts
+++ src/app/core/logout/logout.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../shared/authentication/auth.service';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logout',
@@ -10,11 +10,16 @@ import { AuthService } from '../../shared/authentication/auth.service';
   styleUrl: './logout.component.css'
 })
 export class LogoutComponent {
-   constructor(private auth: AuthService) {
+   constructor(private auth: AuthService, private router: Router) {
     this.auth.SetLoginJwtInfo(false, '');
+    //? aggiunto router che rimanda in home//
+    this.router.navigate(['/home']);
    }
   
+     //TODO verificare se serve davvero questa funzione//
      RunLogout() {
        this.auth.SetLoginJwtInfo(false, '');
+       //? aggiunto router che rimanda in home//
+       this.router.navigate(['/home']);
      }
 }
