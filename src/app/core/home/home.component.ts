66f89ea830ee66977b400b90dd095d3c97fbd6d3
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-12-15 11:34:51 +0100

    added method setActiveindexByName

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { ActiveIndexService } from '../../shared/service/active-index.service';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -9,5 +9,14 @@ import { RouterModule } from '@angular/router';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  constructor(
+    private activeIndexService : ActiveIndexService
+  ){
+  }
+   // Chiamo il metodo del servizio per aggiornare il valore
+  setActiveIndexByName(value: string | null) {
+    console.log('Nuovo indice attivo:', value); 
+    this.activeIndexService.setActiveIndexByName(value);
+  }
 }
+
