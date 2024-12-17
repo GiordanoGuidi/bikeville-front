409b16dfca333d565a755e40a3f46a3aa0c10b9f
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-12-17 12:23:18 +0100

    modified structure app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection ,importProvidersFrom} from '@angular/core';
-import { provideRouter, withRouterConfig } from '@angular/router';
+import { provideRouter} from '@angular/router';
 import { routes } from './app.routes';
 import { HttpClient, HttpClientModule } from '@angular/common/http'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes, withRouterConfig({ scrollPositionRestoration: 'top' })),importProvidersFrom(HttpClientModule, HttpClient)]
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule, HttpClient)
+  ]
 };
