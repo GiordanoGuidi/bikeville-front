7b04d4c2889d6fdcdb67722f2fbf02845b090ff3
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-12-20 11:53:24 +0100

    added file bike-filter-interfaces

---
+++ src/app/features/filters/bike/bike-filter-interfaces.ts
@@ -0,0 +1,9 @@
+// Interfaccia per il filtro del tipo di bicicletta
+export interface BikeTypeFilter {
+    id: number;
+    name: string;
+}
+
+export interface BikeColorFilter{
+    color:string;
+}
