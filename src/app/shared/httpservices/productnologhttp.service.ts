f6f64dadceb7834651f2dc83908b44053cdd02ef
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-12-27 11:27:35 +0100

    added methods to get filtered products

--- src/app/shared/httpservices/productnologhttp.service.ts
+++ src/app/shared/httpservices/productnologhttp.service.ts
@@ -11,15 +11,36 @@ export class ProductnologhttpService {
  
   constructor(private http: HttpClient, private auth: AuthService) { }
 
+  //Funzione per recuperare tutti i prodotti
   getProductnolog(): Observable<any>{
     return this.http.get('https://localhost:7257/api/Products', 
       {headers: this.auth.authenticationJwtHeader}
     );
   }
 
- 
+  //Funzione per recuperare i prodotti in base alla categoria
+  getProductsByParentCategory(parentCategoryId: number): Observable<Product[]> {
+    const params = new HttpParams().set('id', parentCategoryId.toString());
+    return this.http.get<Product[]>('https://localhost:7257/api/Products/by-parent-category', { params });
+  }
 
-  
+  getFilteredProducts(parentCategoryId:number,color?:string|null,typeId?:number|null,size?:string|null):Observable<Product[]>{
+    let params = new HttpParams().set('parentCategoryId', parentCategoryId.toString());
+    if (color) {
+      console.log(color)
+      params = params.set('color', color);
+    }
+    if (typeId) {
+      console.log(typeId)
+      params = params.set('typeId', typeId);
+    }
+    if (size) {
+      console.log(size)
+      params = params.set('size', size);
+    }
+    console.log(params.toString())
+    return this.http.get<Product[]>('https://localhost:7257/api/Products/get-filtered-bikes', { params });
+  }
 }
 
 
