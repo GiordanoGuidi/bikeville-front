d853bf1c3a5dedeea75de8cfca70fe1f7a3d7477
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-12-27 11:24:21 +0100

    added interface bikesizefilter

--- src/app/features/filters/bike/bike-filter-interfaces.ts
+++ src/app/features/filters/bike/bike-filter-interfaces.ts
@@ -3,7 +3,11 @@ export interface BikeTypeFilter {
     id: number;
     name: string;
 }
-
+// Interfaccia per il filtro del colore di bicicletta
 export interface BikeColorFilter{
     color:string;
+}
+// Interfaccia per il filtro della taglia della bicicletta
+export interface BikeSizeFilter{
+    size:string;
 }
