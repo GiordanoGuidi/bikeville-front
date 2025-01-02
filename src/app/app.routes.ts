fc8e2e6d0c00f5eb59e1efaefc196a1779c52ed5
GiordanoGuidi <giordanoguidi23@gmail.com>
2025-01-02 15:49:04 +0100

    added  methods getBikeComponents

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { ProductNologinComponent } from './features/product-nologin/product-nolo
 import { AdminProductsComponent } from './features/admin/admin-products/admin-products.component';
 import { HomeComponent } from './core/home/home.component';
 import { BikesComponent } from './features/products/bikes/bikes.component';
-import { ComponentsComponent } from './features/products/bike-components/bike-components.component';
+import { BikeComponentsComponent } from './features/products/bike-components/bike-components.component';
 import { ClothingComponent } from './features/products/clothing/clothing.component';
 import { AccessoriesComponent } from './features/products/accessories/accessories.component';
 
@@ -20,7 +20,7 @@ export const routes: Routes = [
     {path:'adminproducts',component:AdminProductsComponent},
     {path:'home',component:HomeComponent},
     {path:'Bikes',component:BikesComponent},
-    {path:'Components',component:ComponentsComponent},
+    {path:'Components',component:BikeComponentsComponent},
     {path:'Clothing',component:ClothingComponent},
     {path:'Accessories',component:AccessoriesComponent},
 ];
--- src/app/features/products/bike-components/bike-components.component.ts
+++ src/app/features/products/bike-components/bike-components.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../../shared/Models/products';
+import { HttpClient, HttpHeaders ,HttpParams} from '@angular/common/http';
+import { ProductnologhttpService } from '../../../shared/httpservices/productnologhttp.service';
 
 @Component({
   selector: 'app-components',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './bike-components.component.html',
   styleUrl: './bike-components.component.css'
 })
-export class ComponentsComponent {
+export class BikeComponentsComponent {
+  constructor(
+    private http:HttpClient,
+    private httpRequest:ProductnologhttpService
+  ){}
+  //#Dates
+  //Assegno l'id della parentcategory delle biciclette
+  parentCategoryId = 2; 
+  //array dei componenti
+  components :Product[]=[]
 
+  //#Function
+  //Recupero tutti i componenti delle biciclette
+  getBikeComponents():void{
+    this.httpRequest.getProductsByParentCategory(this.parentCategoryId).subscribe({
+      next:(data)=>{
+        this.components=data;
+        console.log(this.components)
+      },
+      error: (err) => {
+        console.error('Error:', err);
+      },
+    });
+  }
+
+  ngOnInit():void{
+    this.getBikeComponents();
+  }
 }
