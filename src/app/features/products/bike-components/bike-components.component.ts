8f8a69233b28f2b2ce9dd92339ca75a8c375262d
GiordanoGuidi <giordanoguidi23@gmail.com>
2025-01-04 10:21:37 +0100

    added sidebar component into bike-components

--- src/app/features/products/bike-components/bike-components.component.ts
+++ src/app/features/products/bike-components/bike-components.component.ts
@@ -1,14 +1,14 @@
 import { Component,OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../../shared/Models/products';
-import { TypeFilter,ColorFilter,SizeFilter, PriceFilter } from '../../filters/bike/bike-filter-interfaces';
+import { TypeFilter,ColorFilter,SizeFilter, PriceFilter } from '../../filters/bike-filter-interfaces';
 import { HttpClient, HttpHeaders ,HttpParams} from '@angular/common/http';
 import { ProductnologhttpService } from '../../../shared/httpservices/productnologhttp.service';
-
+import { SidebarComponent } from '../../../shared/components/sidebar/sidebar.component';
 @Component({
   selector: 'app-components',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,SidebarComponent],
   templateUrl: './bike-components.component.html',
   styleUrl: './bike-components.component.css'
 })
@@ -98,7 +98,7 @@ export class BikeComponentsComponent {
       let coercedValue = filterType === 'typeId' || filterType === 'price' ? Number(value) : value;
       // Aggiungo il nuovo filtro
       this.activeFilter.push({ filterType, value:coercedValue });
-      
+      console.log(this.activeFilter)
       // Aggiorno i valori selezionati
       if (filterType === 'color') this.selectedColor = value;
       if (filterType === 'typeId') this.selectedType = Number(value);
@@ -130,7 +130,85 @@ export class BikeComponentsComponent {
     })
   }
 
+  //! Rimuovere alla fine?
+   // Metodo per verificare se un filtro è attivo
+   isFilterActive(filterType: string, value: string|number): boolean {
+    const coercedValue = filterType === 'typeId' || filterType === 'price' ? Number(value) : value;
+    return this.activeFilter.some(filter => filter.filterType === filterType && filter.value === coercedValue);
+  }
 
+  //! Rimuovere alla fine?
+  //Metodo per rimuovere un filtro attivo
+  removeFilter(filterType:string){
+    //Resetto i valore in base al tipo di filtro 
+    switch(filterType){
+      case'color':this.selectedColor = null;
+      break;
+      case'typeId':this.selectedType = null;
+      break;
+      case'size':this.selectedSize = null;
+      break;
+      case 'price':this.selectedPrice = null;
+      break;
+      default:
+        console.log('Filtro non valido');
+    }
+    // Richiesta HTTP per filtrare le biciclette
+    this.httpRequest.getFilteredProducts(this.parentCategoryId,this.selectedColor,this.selectedType,this.selectedSize,this.selectedPrice).subscribe({
+      next:(data)=>{
+        this.components = data;
+        console.log('filtri attivi:',this.activeFilter)
+      },
+      error: (err) => {
+        console.error('Error:', err);
+      },
+    })
+    //Rimuovo il filtro dall array di filtri attivi
+    this.activeFilter = this.activeFilter.filter(filter => filter.filterType !== filterType);
+  }
+
+  //! Rimuovere alla fine?
+  //Metodo che restituisce la stringa di tipologia di bicicletta dal suo id
+  getActiveFilter(value: number): string | number {
+    const typeMap1: { [key: number]: string } = {
+      1: 'Up to 100€',
+      2: '100-500€',
+      3: '500-100€',
+      4: '1000€ and more',
+    };
+  
+    const typeMap2: { [key: number]: string } = {
+      8: 'Handlebars',
+      9: 'Bottom Brackets',
+      10: 'Brakes',
+      11: 'Chains',
+      12: 'Cranksets',
+      13: 'Derailleurs',
+      14: 'Forks',
+      15: 'Headsets',
+      16: 'Mountain Frames',
+      17: 'Pedals',
+      18: 'Road Frames',
+      19: 'Saddles',
+      20: 'Touring Frames',
+      21: 'Wheels',
+    };
+  
+    switch (true) {
+      case value >= 1 && value <= 4:
+        return typeMap1[value as number] || 'Unknown Type';
+      case value >= 8 && value <= 21:
+        return typeMap2[value as number] || 'Unknown Type';
+      default:
+        return value;
+    }
+  }
+
+  //! Rimuovere alla fine?
+  //Metodo per restituire una stringa in minuscolo
+  toLower(input: string): string {
+    return input.replace(/\//g, '-').toLowerCase();
+  }
 
   ngOnInit():void{
     this.getBikeComponents();
