0fd8f0149302a3b324ed1527a3347e04a53fee18
GiordanoGuidi <giordanoguidi23@gmail.com>
2025-01-08 10:32:08 +0100

    added router events into pagination-service

--- src/app/shared/service/pagination-service.ts
+++ src/app/shared/service/pagination-service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaginationService {
+
+  constructor(private router: Router) {
+    //stream di eventi emessi dal router quando cambia stato
+    this.router.events
+    //filtro gli eventi navigationend
+      .pipe(filter(event => event instanceof NavigationEnd))
+      //se l'evento di tipo navigationend esiste eseguo il codice
+      .subscribe((event:NavigationEnd) => {
+        this.setCurrentPage(1);
+      });
+  }
+  
   //istanza BehaviorSubject con valore iniziale 1
   private currentPageSubject = new BehaviorSubject<number>(1);
   //Converto il BehaviorSubject in un Observable
