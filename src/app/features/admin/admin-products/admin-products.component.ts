63ba72c28775e58e61f8976b19f516db26624790
GiordanoGuidi <giordanoguidi23@gmail.com>
2025-01-16 19:08:37 +0100

    added alert on components

--- src/app/features/admin/admin-products/admin-products.component.ts
+++ src/app/features/admin/admin-products/admin-products.component.ts
@@ -12,6 +12,7 @@ import { ModalSessionService } from '../../../shared/service/modal-session.servi
 import { SessionModalComponent } from '../../../shared/components/session-modal/session-modal/session-modal.component';
 import { AlertComponent } from '../../../shared/components/alert/alert/alert.component';
 import { AlertService } from '../../../shared/service/alert.service';
+
 declare const $: any;
 
 
@@ -67,7 +68,20 @@ export class AdminProductsComponent {
   }
 
 // funzione per aggiungere un prodotto//
-addProduct(prodotto: NgForm) {
+async addProduct(prodotto: NgForm) {
+   //Verifico la validità del token
+   const tokenCheckResponse = await fetch("https://localhost:7257/api/Customers/ValidateAdminToken", {
+    method: "GET",
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem("jwtToken")}`, 
+    },
+  });
+  //Se il token non è valido faccio rieseguire il login
+  if (!tokenCheckResponse.ok) {
+    console.error("Token non valido o scaduto");
+    this.modalService.openModal();
+    return;
+  }
   this.newProduct = prodotto.value;
   console.log(this.newProduct);
   this.httpRequest.postAdminProduct(this.newProduct).subscribe({
