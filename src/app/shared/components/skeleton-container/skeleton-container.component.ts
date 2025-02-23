9b810779460a41b51a15671d454e7be69d955a0b
GiordanoGuidi <giordanoguidi23@gmail.com>
2025-02-23 18:21:32 +0100

    created skeleton-container component

---
+++ src/app/shared/components/skeleton-container/skeleton-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './skeleton-container.component.html',
+  styleUrl: './skeleton-container.component.css'
+})
+export class SkeletonContainerComponent {
+
+}
